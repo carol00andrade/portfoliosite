6b15f2a7dbb722544c7e033e5c6bd5ce84e9633d
Ana Carolina Andrade <144967701+carol00andrade@users.noreply.github.com>
2024-06-07 16:07:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('portfortfolio.html')
+    return render_template('portfolio.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
